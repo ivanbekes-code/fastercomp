211b177743d1509b8997e161483ba2c4c95e5cc1
ivanbekes-code <ivan.bekes@gmail.com>
2026-01-15 00:58:16 +0100

    Tidy responsive styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
+const nav = document.querySelector(".nav-links");
 
 if (navToggle && nav) {
   navToggle.addEventListener("click", () => {
